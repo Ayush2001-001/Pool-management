683bdce03765f9cd6a0f7feedee79a07b40c565d
Ayush2001 <ayushkumar@tothebyte.com>
2025-09-16 12:35:35 +0530

    providerName changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,7 @@
-
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { CookiesProvider } from "next-client-cookies/server";
-import Provider from  "./provider";
-
+import StateProvider from "./state.provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,13 +22,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <Provider store={store}>
-          
-        <CookiesProvider >
-          {children}
-        </CookiesProvider>
-        </Provider>
-
+        <StateProvider>
+          <CookiesProvider>{children}</CookiesProvider>
+        </StateProvider>
       </body>
     </html>
   );
